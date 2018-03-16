e684d1c3a7dc40b0131ac1ed4303aea75519f863
Rigney <krigney@proforma.com>
2018-03-16 16:35:00 -0400

    Solid Framework, I think. Working on totaling player values

--- public/main.js
+++ public/main.js
@@ -1,3 +1,5 @@
+//SHUFFLE
+
 // Build a deck of 52 cards
 let suitOptions = ["hearts", "spades", "clubs", "diamonds"];
 let numberOptions = ["ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king"];
@@ -7,7 +9,7 @@ let numberSelected = "";
 let card = [];
 
 const shuffleDeck = () => {
-  console.log("Button was clicked");
+  // console.log("Button was clicked");
   for (let i = 0; i < suitOptions.length; i++) {
     suitSelected = suitOptions[i];
     // console.log(suitSelected);
@@ -22,43 +24,183 @@ const shuffleDeck = () => {
   console.log(thisDeck);
 }
 
-// // assign values to all 52 cards
-// //      - 2-10 = face value
-// //      - K, Q, J = 10
-// //      - A = 11
 
 
 // // Player = user
 // // House = computer
 
+
+
+//DEAL
+
 let houseHand = [];
 let playerHand = [];
 
-
 const dealButtonClick = () => {
-  console.log("Button was clicked");
   for (i = 0; i <= 1; i++) {
-    card = [Math.floor(Math.random() * thisDeck.length)];
-    finalcard = thisDeck[card];
-    houseHand.push(finalcard);
+    arrayItemPull = [Math.floor(Math.random() * thisDeck.length)];
+    finalCard = thisDeck[arrayItemPull];
+    houseHand.push(finalCard);
   }
   document.querySelector('#houseDealtCards').textContent = houseHand;
   console.log(houseHand);
-
-  // //       - house cards are hidden until reveal
-
+  // // - house cards are hidden until reveal
   for (i = 0; i <= 1; i++) {
-    card = [Math.floor(Math.random() * thisDeck.length)];
-    finalcard = thisDeck[card];
-    playerHand.push(finalcard);
+    arrayItemPull = [Math.floor(Math.random() * thisDeck.length)];
+    finalCard = thisDeck[arrayItemPull];
+    playerHand.push(finalCard);
   }
   document.querySelector('#playerDealtCards').textContent = playerHand;
   console.log(playerHand);
-
-  //   evalScore();
+  cardValuesAfterDeal();
 }
 
-// // const evalScore =  () =>{
+
+
+//CARD VALUES
+
+let playerScore = 0;
+let houseScore = 0;
+let playerScoreTotal = [];
+let houseScoreTotal = [];
+
+const cardValuesAfterDeal = () => {
+  for (i = 0; i < playerHand.length; i++) {
+    if (playerHand[i].includes("ace")) {
+      console.log ("+11")
+      newPlayerScore = playerScore + 11;
+    }
+    if (playerHand[i].includes("queen")) {
+      console.log ("+10")
+      newPlayerScore = playerScore + 10;
+    }
+    if (playerHand[i].includes("king")) {
+      console.log ("+10")
+      newPlayerScore = playerScore + 10;
+    }
+    if (playerHand[i].includes("jack")) {
+      console.log ("+10")
+      newPlayerScore = playerScore + 10;
+    }
+    if (playerHand[i].includes("2")) {
+      console.log ("+2")
+      newPlayerScore = playerScore + 2;
+    }
+    if (playerHand[i].includes("3")) {
+      console.log ("+3")
+      newPlayerScore = playerScore + 3;
+    }
+    if (playerHand[i].includes("4")) {
+      console.log ("+4")
+      newPlayerScore = playerScore + 4;
+    }
+    if (playerHand[i].includes("5")) {
+      console.log ("+5")
+      newPlayerScore = playerScore + 5;
+    }
+    if (playerHand[i].includes("6")) {
+      console.log ("+6")
+      newPlayerScore = playerScore + 6;
+    }
+    if (playerHand[i].includes("7")) {
+      console.log ("+7")
+      newPlayerScore = playerScore + 7;
+    }
+    if (playerHand[i].includes("8")) {
+      console.log ("+8")
+      newPlayerScore = playerScore + 8;
+    }
+    if (playerHand[i].includes("9")) {
+      console.log ("+9")
+      newPlayerScore = playerScore + 9;
+    }
+    if (playerHand[i].includes("10")) {
+      console.log ("+10")
+      newPlayerScore = playerScore + 10;
+    }
+    playerScoreTotal.push(newPlayerScore);
+    console.log(playerScoreTotal);
+  }
+  for (i = 0; i < houseHand.length; i++) {
+    if (houseHand[i].includes("ace")) {
+      console.log ("+11")
+      newHouseScore = houseScore + 11;
+    }
+    if (houseHand[i].includes("queen")) {
+      console.log ("+10")
+      newHouseScore = houseScore + 10;
+    }
+    if (houseHand[i].includes("king")) {
+      console.log ("+10")
+      newHouseScore = houseScore + 10;
+    }
+    if (houseHand[i].includes("jack")) {
+      console.log ("+10")
+      newHouseScore = houseScore + 10;
+    }
+    if (houseHand[i].includes("2")) {
+      console.log ("+2")
+      newHouseScore = houseScore + 2;
+    }
+    if (houseHand[i].includes("3")) {
+      console.log ("+3")
+      newHouseScore = houseScore + 3;
+    }
+    if (houseHand[i].includes("4")) {
+      console.log ("+4")
+      newHouseScore = houseScore + 4;
+    }
+    if (houseHand[i].includes("5")) {
+      console.log ("+5")
+      newHouseScore = houseScore + 5;
+    }
+    if (houseHand[i].includes("6")) {
+      console.log ("+6")
+      newHouseScore = houseScore + 6;
+    }
+    if (houseHand[i].includes("7")) {
+      console.log ("+7")
+      newHouseScore = houseScore + 7;
+    }
+    if (houseHand[i].includes("8")) {
+      console.log ("+8")
+      newHouseScore = houseScore + 8;
+    }
+    if (houseHand[i].includes("9")) {
+      console.log ("+9")
+      newHouseScore = houseScore + 9;
+    }
+    if (houseHand[i].includes("10")) {
+      console.log ("+10")
+      newHouseScore = houseScore + 10;
+    }
+    houseScoreTotal.push(newHouseScore);
+    console.log(houseScoreTotal);
+  }
+  playerAddTotal();
+}
+
+
+
+// let playerSum = playerScoreTotal.reduce(a,b) => a + b;
+
+// let sumStart = 0;
+// let playerSum = [];
+
+// const playerAddTotal = () => {
+//   console.log("Button was clicked")
+//   for (let i=0; i < playerScoreTotal.length; i++) {
+//     playerSum = sumStart + playerScoreTotal[i];
+//   }
+//   return playerSum;
+//   console.log (playerSum);
+// }
+
+
+
+
+// const evalScore =  () =>{
+
 //   if (houseSum > 21){
 //     alert("House busts!")
 //   } 
@@ -75,14 +217,16 @@ const dealButtonClick = () => {
 //   //if either house or player over 21 = alert bust
 //   //if either house or player equals 21 - alert winner
 //  //else keep playing 
-// }
+
 
 const hitButtonClick = () => {
   console.log("Button was clicked");
-  card = [Math.floor(Math.random() * thisDeck.length)];
-  playerHand.push(card);
+  arrayItemPull = [Math.floor(Math.random() * thisDeck.length)];
+  finalCard = thisDeck[arrayItemPull];
+  playerHand.push(finalCard);
   document.querySelector('#playerDealtCards').textContent = playerHand;
   console.log(playerHand);
+  cardValuesAfterHit();
 
   // // player gets another card from the remaining deck until they stop hitting or  // after each hit evalScore()
 }
@@ -91,16 +235,21 @@ const stayButtonClick = () => {
   console.log("Button was clicked");
   //evalScore();
   housePlay();
- // // when player is done evaluate card sum and store
+  cardValues();
+
+  // // when player is done evaluate card sum and store
 }
 
 const housePlay = () => {
   while (houseScore < 18) { //DEFINE houseScore //
-    card = [Math.floor(Math.random() * thisDeck.length)];
-    houseHand.push(card);
+    arrayItemPull = [Math.floor(Math.random() * thisDeck.length)];
+    finalCard = thisDeck[arrayItemPull];
+    houseHand.push(finalCard);
   }
   document.querySelector('#houseDealtCards').textContent = houseHand;
   console.log(houseHand);
+  cardValues();
+
 }
 
 
